77d13041dfb7bdca221638a92f6653dbad0ad3b0
ATHUL-KRISHANA <athulpadiyath@gmail.com>
2025-12-18 15:02:39 +0530

    create document related api

--- main.py
+++ main.py
@@ -34,12 +34,14 @@ from fastapi import FastAPI
 from app.routers import user
 from app.routers import upload
 from app.routers import chat
+from app.routers import dashboard
 
 app = FastAPI()
 
 app.include_router(user.router)
 app.include_router(upload.router)
 app.include_router(chat.router)
+app.include_router(dashboard.router)
 
 @app.get("/")
 def root():
